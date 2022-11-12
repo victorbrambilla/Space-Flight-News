cc8a3fb745bfc28bc56a08d4799c5fc9ecb685ec
victor <victor-brambilla@hotmail.com>
2022-11-12 19:15:57 -0400

    feat: Create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { ArticleModel } from '@/domain/models';
+import { MakeRemoteGetArticles } from '@/main/factories/usecases';
 import { Home } from '../presentation/views/Home';
 
-export default function HomePage() {
-  return <Home />;
+interface IProps {
+  data: ArticleModel[];
+}
+
+export default function HomePage({ data }: IProps) {
+  return <Home data={data} />;
+}
+
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const data = await MakeRemoteGetArticles().perform({
+    _limit: 10,
+    _start: page,
+  });
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
--- src/presentation/views/Home.tsx
+++ src/presentation/views/Home.tsx
@@ -1,5 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { ArticleModel } from '@/domain/models';
+import { CardNews } from '@/presentation/components/home/CardNews';
+import { Header } from '@/presentation/components/home/Header';
+import { Box, Button } from '@mui/material';
+import { MakeRemoteGetArticles } from '@/main/factories/usecases';
+import { GetArticles } from '@/domain/usecases';
+import useDebounce from '@/presentation/hooks/useDebounce';
 
-export const Home = () => {
-  return <div>Home</div>;
+interface IProps {
+  data: ArticleModel[];
+}
+
+export const Home = ({ data }: IProps) => {
+  const [params, setParams] = React.useState<GetArticles.Params>({
+    _limit: 10,
+    _start: 1,
+  });
+  const [isFirstRender, setIsFirstRender] = React.useState(true);
+  const [news, setNews] = React.useState<ArticleModel[]>();
+  const debouncedSearchTerm = useDebounce(params, 500);
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setNews(data);
+    } else {
+      if (debouncedSearchTerm !== '') {
+        console.log('sasd');
+        MakeRemoteGetArticles()
+          .perform(params)
+          .then((res) => {
+            setNews(res);
+          });
+      } else {
+        MakeRemoteGetArticles()
+          .perform(params)
+          .then((res) => {
+            console.log(res);
+            setNews(res);
+          });
+      }
+    }
+
+    if (isFirstRender) setIsFirstRender(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchTerm]);
+
+  return (
+    <Box
+      sx={{
+        backdropFilter: 'brightness(0.5)',
+        minHeight: '100vh',
+      }}
+    >
+      <Header setParams={setParams} params={params} />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          gap: '50px',
+          height: '100%',
+          marginTop: '100px',
+          padding: '0 20px',
+          '@media (max-width:800px)': {
+            padding: '0 ',
+          },
+        }}
+      >
+        {news?.map((item, i) => {
+          return <CardNews key={item.id} news={item} index={i} />;
+        })}
+      </Box>
+    </Box>
+  );
 };
