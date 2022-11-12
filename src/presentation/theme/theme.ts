97703308d7caebc3d9d71a1112268ca86f5b7932
victor <victor-brambilla@hotmail.com>
2022-11-12 14:48:12 -0400

    feat: Create Theme

---
+++ src/presentation/theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#302E53',
+    },
+    secondary: {
+      main: '#D07017',
+    },
+  },
+});
