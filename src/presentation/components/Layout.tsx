7a291294d7c1c055da3539d6f53313a2fbdb6be5
victor <victor-brambilla@hotmail.com>
2022-11-12 15:52:58 -0400

    style: Add Background image to the app

--- src/presentation/components/Layout.tsx
+++ src/presentation/components/Layout.tsx
@@ -11,6 +11,11 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
         style={{
           maxWidth: '100%',
           minHeight: '100vh',
+          backgroundImage:
+            'url(https://images.unsplash.com/photo-1505506874110-6a7a69069a08?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
       >
         {children}
