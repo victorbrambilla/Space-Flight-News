8691816431bd46238e0f2e7e337787670a6480da
victor <victor-brambilla@hotmail.com>
2022-11-13 15:06:24 -0400

    feat: Create pagination function

--- src/presentation/views/Home.tsx
+++ src/presentation/views/Home.tsx
@@ -6,6 +6,7 @@ import { Box, Button } from '@mui/material';
 import { MakeRemoteGetArticles } from '@/main/factories/usecases';
 import { GetArticles } from '@/domain/usecases';
 import useDebounce from '@/presentation/hooks/useDebounce';
+import { LoadingDots } from '@/presentation/components/home/LoadingDots';
 
 interface IProps {
   data: ArticleModel[];
@@ -18,30 +19,29 @@ export const Home = ({ data }: IProps) => {
   });
   const [isFirstRender, setIsFirstRender] = React.useState(true);
   const [news, setNews] = React.useState<ArticleModel[]>();
+  const [loading, setLoading] = React.useState(false);
   const debouncedSearchTerm = useDebounce(params, 500);
 
   useEffect(() => {
+    setLoading(true);
+
     if (isFirstRender) {
       setNews(data);
+      setLoading(false);
+      setIsFirstRender(false);
     } else {
-      if (debouncedSearchTerm !== '') {
-        console.log('sasd');
+      if (debouncedSearchTerm) {
         MakeRemoteGetArticles()
           .perform(params)
           .then((res) => {
             setNews(res);
-          });
-      } else {
-        MakeRemoteGetArticles()
-          .perform(params)
-          .then((res) => {
-            console.log(res);
-            setNews(res);
+          })
+          .finally(() => {
+            setLoading(false);
           });
       }
     }
 
-    if (isFirstRender) setIsFirstRender(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedSearchTerm]);
 
@@ -71,6 +71,37 @@ export const Home = ({ data }: IProps) => {
           return <CardNews key={item.id} news={item} index={i} />;
         })}
       </Box>
+      {loading && <LoadingDots />}
+      {!!news?.length && news ? (
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          <Button
+            variant='outlined'
+            size='large'
+            sx={{
+              margin: '50px 0',
+            }}
+            disabled={loading}
+            onClick={() => {
+              setParams({
+                ...params,
+                _limit: params._limit + 10,
+              });
+            }}
+          >
+            Carregar Mais
+          </Button>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            width: '100%',
+
+            textAlign: 'center',
+          }}
+        >
+          <h1>Não há notícias para essa busca</h1>
+        </Box>
+      )}
     </Box>
   );
 };
