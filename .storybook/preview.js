bff45f1b688f5b06635c24784d111a08570ca6c1
victor <victor-brambilla@hotmail.com>
2022-12-10 14:48:15 -0400

    chore: Init storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import {theme} from '../src/presentation/theme';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <CssBaseline />
+    </ThemeProvider>
+  )
+];
